0a0b69dd5ecbfe4bdc97f6b16bda6b640d47adbf
Robson Henrique Rodrigues <robhenrod@gmail.com>
2024-06-17 12:52:16 -0300

    fix: cors config

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -6,6 +6,10 @@ import { EnvService } from './env/env.service';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: '*',
+  });
+
   const config = new DocumentBuilder()
     .setTitle('RamenGO!')
     .setDescription(
